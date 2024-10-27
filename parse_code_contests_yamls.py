c31cb233f78e5449d87ca1f343fa672562dcc7ff
Jon Saad-Falcon <jonsaadfalcon@gmail.com>
2024-10-27 22:26:23 +0000

    add codecontests

--- parse_code_contests_yamls.py
+++ parse_code_contests_yamls.py
@@ -178,8 +178,6 @@ def main(input_directory: str, output_filepath: str):
     
     logging.info(f"Dataset saved successfully. Total samples: {len(dataset)}")
 
-    breakpoint()
-
 if __name__ == "__main__":
     save_dir = os.environ.get('SAVE_DIR')
     if not save_dir:
